00b401753c5c6c68ec5c81074387baf407bd89f0
Shade50 <labbe.jerome@gmail.com>
2026-07-13 17:47:25 +0200

    (feature) - mise en place de l'historique des commandes dans le profil utilisateur

--- vite-gourmand/src/Controller/ProfileController.php
+++ vite-gourmand/src/Controller/ProfileController.php
@@ -50,6 +50,7 @@ final class ProfileController extends AbstractController
         }
         return $this->render('profile/index.html.twig', [
             'form' => $form,
+            'orders' => $user->getOrders(),
 
         ]);
     }
