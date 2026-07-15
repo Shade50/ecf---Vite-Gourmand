857c3c3679eaa8f873dafbf9d52d4d6b341ffdf4
Shade50 <labbe.jerome@gmail.com>
2026-07-15 11:08:03 +0200

    feat(employee): - création du tableau de bord employé

---
+++ vite-gourmand/src/Controller/EmployeeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\OrderRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+
+
+final class EmployeeController extends AbstractController
+{
+    #[Route('/employee', name: 'app_employee_index', methods: ['GET'])]
+    public function index(
+        Request $request,
+        OrderRepository $orderRepository,
+    ): Response {
+        $this->denyAccessUnlessGranted('ROLE_EMPLOYER');
+
+        $search = trim((string) $request->query->get('search', ''));
+        $status = trim((string) $request->query->get('status', ''));
+
+        /* premiere version simple :
+         * on affiche tous
+         * je brancherai la recherche et le filtre dans le repository plus tard
+         */
+
+        $orders = $orderRepository->findBy(
+            [],
+            ['createAt' => 'DESC']
+        );
+
+
+        return $this->render('employee/index.html.twig', [
+            'orders' => $orders,
+            'search' => $search,
+            'currentStatus' => $status,
+            'statuses' => [
+                'En attente',
+                'Acceptée',
+                'en préparation',
+                'En cours de livraison',
+                'En attente du retour de matériel',
+                'Terminée',
+                'Annulée'
+            ],
+        ]);
+    }
+}
