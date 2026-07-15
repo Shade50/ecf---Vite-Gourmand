528e2241c1d57b0c0d422c9eaef831046dfd7a3b
Shade50 <labbe.jerome@gmail.com>
2026-07-15 11:35:40 +0200

    (feature)employer -mise en place du systreme de filtre de la barre de recherche

--- vite-gourmand/src/Repository/OrderRepository.php
+++ vite-gourmand/src/Repository/OrderRepository.php
@@ -16,28 +16,55 @@ class OrderRepository extends ServiceEntityRepository
         parent::__construct($registry, Order::class);
     }
 
-//    /**
-//     * @return Order[] Returns an array of Order objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('o')
-//            ->andWhere('o.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('o.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findForEmployee(?string $search, ?string $status): array
+    {
+        $qb = $this->createQueryBuilder('o')
+            ->leftJoin('o.user', 'u')
+            ->leftJoin('o.menu', 'm')
+            ->addSelect('u', 'm')
+            ->orderBy('o.deliveryDate', 'ASC');
 
-//    public function findOneBySomeField($value): ?Order
-//    {
-//        return $this->createQueryBuilder('o')
-//            ->andWhere('o.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        if (!empty($search)) {
+            $qb
+                ->andWhere(
+                    'u.nom LIKE :search
+                OR u.prenom LIKE :search
+                OR u.email LIKE :search'
+                )
+                ->setParameter('search', '%' . $search . '%');
+        }
+
+        if (!empty($status)) {
+            $qb
+                ->andWhere('o.status = :status')
+                ->setParameter('status', $status);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+    //    /**
+    //     * @return Order[] Returns an array of Order objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('o')
+    //            ->andWhere('o.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('o.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Order
+    //    {
+    //        return $this->createQueryBuilder('o')
+    //            ->andWhere('o.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
