d86ac14ddb1a592926c8e74afcbc945e05673edd
Shade50 <labbe.jerome@gmail.com>
2026-07-22 14:02:44 +0200

    (feature) : - ajout des avis sur la home limité a 4 avis - mise a jours des bouton retour (admin et employee)

--- vite-gourmand/src/Controller/HomeController.php
+++ vite-gourmand/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ReviewRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,61 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(
+        ReviewRepository $reviewRepository
+    ): Response {
+        $reviews = $reviewRepository->findBy(
+            ['statut' => 'approved'],
+            ['id' => 'DESC'],
+            4
+        );
+
         return $this->render('home/index.html.twig', [
-
-    'menus' => [
-
-        [
-            'titre' => 'Menu Mariage',
-            'description' => 'Cocktail + repas complet',
-            'prix' => 45
-        ],
-
-        [
-            'titre' => 'Menu Entreprise',
-            'description' => 'Buffet froid',
-            'prix' => 25
-        ],
-
-        [
-            'titre' => 'Menu Anniversaire',
-            'description' => 'Repas convivial',
-            'prix' => 30
-        ],
-
-        [
-            'titre' => 'Menu pâque',
-            'description' => 'Repas convivial très complet',
-            'prix' => 45
-        ],
-
-    ],
-
-    'avis' => [
-
-        [
-            'nom' => 'Jean Dupont',
-            'note' => 5,
-            'commentaire' => 'Excellent service et plats délicieux !'
-        ],
-
-        [
-            'nom' => 'Marie Martin',
-            'note' => 4,
-            'commentaire' => 'Très bon rapport qualité/prix.'
-        ],
-
-        [
-            'nom' => 'Pierre Durand',
-            'note' => 3,
-            'commentaire' => 'Plats corrects mais service un peu lent.'
-        ],
-
-    ]
-
+            'reviews' => $reviews,
         ]);
-
     }
 }
